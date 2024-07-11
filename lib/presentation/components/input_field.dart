dea61f99ca8227a2210285ead0f4a6f537e9ab80
lgy159 <lgy159@naver.com>
2024-07-11 23:39:21 +0900

    feat: 디자인 시스템 구축

---
+++ lib/presentation/components/input_field.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class InputField extends StatelessWidget {
+  final String label;
+  final String place_holder;
+
+  const InputField(
+      {super.key, required this.label, required this.place_holder});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Label',style: ,),
+
+      ],
+    );
+  }
+}
---
+++ lib/presentation/sing_up/sing_up_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:sesac_ton/presentation/components/input_field.dart';
+
+class SingUpScreen extends StatelessWidget {
+  const SingUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: InputField(),
+    );
+  }
+}
